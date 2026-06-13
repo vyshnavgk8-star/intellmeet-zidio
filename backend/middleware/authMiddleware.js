9c4d9f462b053acb9c7d94232330814ca825a6ed
vyshnavgk8-star <vyshnavgk8@gmail.com>
2026-06-13 21:41:13 +0530

    feat: complete JWT authentication system

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({
+            message: "No token provided",
+        });
+    }
+
+    try {
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        res.status(401).json({
+            message: "Invalid token",
+        });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", require("./routes/authRoutes"));
+
 app.get("/", (req, res) => {
     res.send("IntellMeet Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
+
+const authMiddleware = require("./middleware/authMiddleware");
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+    res.json({
+        message: "Protected Route Accessed",
+        user: req.user,
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
